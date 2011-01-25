c72cb4bcdd0ad046a9e26d51b9683b33b3915004
vxsx <vxsx@me.com>
2011-01-25 17:12:16 +0300

    changed styling && dragging now works ( with clicks )

--- js/switch.js
+++ js/switch.js
@@ -1,11 +1,35 @@
 $(function() {
-    var s = $('.super-switch').css('display', 'none');
+    var s = $('.super-switch').css('display', 'none'),
+		w = $('.super-switch-wrap'),
+		$true =  $('.true', w),
+		$false =  $('.false', w),
+		knob = $('.knob', w);
 
-    $('.knot').draggable({
+		if ( $true.text().length > $false.text().length ) {
+			var offset = $true.width() + 20;
+			$false.width( $true.width() );
+		} else {
+			var offset =  $false.width() + 20;
+			$true.width( $false.width() );			
+		}
+		
+		w.css({
+			'width': offset + 30
+		})
+		knob.css({
+			'left': offset
+		})
+	
+	knob.click(function(){
+		$(this).animate({'left': ( parseInt($(this).css('left')) > 0 ) ? 0 : offset  })
+		$('.super-switch-wrap .label-wrap').animate({'left': ( parseInt($(this).css('left')) > 0 ) ?  - offset : 0 });
+	})
+	
+    knob.draggable({
         containment: 'parent',
         axis: 'x',
         drag: function(event, ui) {
-			$('.super-switch-wrap .label-wrap').css({'left': -40 + ui.position.left + 'px'});
+			$('.super-switch-wrap .label-wrap').css({'left': - offset + ui.position.left + 'px'});
 		}
     })
 
